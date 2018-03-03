0a4481b113edfe615d28d4d336ecb2d4b1d92c27
Tarnation93 <vukasinkreten@yahoo.com>
2018-03-03 18:03:08 +0100

    first commit

---
+++ src/Components/Navigation.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navigation = () => {
+return (
+	
+	<nav>
+		<p className = 'f3 i dark-blue tl pointer link dim'>Sign Out</p>
+	</nav>
+)
+}
+
+export default Navigation;
